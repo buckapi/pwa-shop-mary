7b663538e890a5d45b01db3798de7fd93f8d26ba
root <root@localhost.localdomain>
2024-04-19 04:09:07 -0500

    shop

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -17,5 +17,27 @@ export class ShopComponent {
     public yeoman: Yeoman,
     public global: GlobalService,
     public dataApiService: DataApiService
-  ){}
+  ){
+    this.getAllProducts();
+    this.getAllCategories();
+  }
+  setPreview(i:any){
+    this.yeoman.preview=this.yeoman.products[i];
+   /*  this.virtualRouter.navigate(['detail']); */
+  }
+  getAllProducts(){
+ 
+    this.dataApiService.getAllProducts().subscribe(response=>{
+      this.yeoman.products=response;
+      console.log('getAllProducts() se ha llamado correctamente.');
+  
+    });
+  }
+  getAllCategories() {
+    this.dataApiService.getAllCategory().subscribe(response => {
+     /*  this.yeoman.categories = response; */
+      this.yeoman.allcategory = response;
+  /*     this.yeoman.allCategoriesSize = this.yeoman.categories.length;
+   */  });
+  }
 }
